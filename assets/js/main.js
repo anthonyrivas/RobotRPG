74d378c140b304fa54c1e7fc8084310d3074f962
Anthony Rivas <kamikazebot@gmail.com>
2018-02-17 21:18:20 -0700

    Adding name and stat display / minimal refactoring

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,12 @@
-$( document ).ready( function () {
-    var unselectedCharactersContainer = $( '.characters' );
-    var unselectedEnemysContainer = $( '.enemy-characters' );
-    var activeBattleContainer = $( '.active-battle' );
-
+$(document).ready(function () {
     var game = {
         characters: [],
-        newCharacter: function ( name, hp, src, minAtk, maxAtk ) {
+        domContainers: {
+            unselectedCharactersContainer: $('.characters'),
+            unselectedEnemysContainer: $('.enemy-characters'),
+            activeBattleContainer: $('.active-battle')
+        },
+        createCharacter: function (name, hp, src, minAtk, maxAtk) {
             var char = {
                 "name": name,
                 "hp": hp,
@@ -14,48 +15,56 @@ $( document ).ready( function () {
                 "maximumAttack": maxAtk,
                 "div": null
             };
-            var newDiv = $( '<div></div>' );
-            newDiv.addClass( 'unselected-character character col-2 p-3 m-3' );
-            var img = $( '<img/>' );
-            img.attr( 'class', 'img-fluid character-image' );
-            img.attr( 'src', src );
-            newDiv.append( img );
-            newDiv.data( 'name', name );
-            newDiv.data( 'hp', hp );
-            newDiv.data( 'minAtk', minAtk );
-            newDiv.data( 'maxAtk', maxAtk );
+            var newDiv = $('<div></div>');
+            newDiv.addClass('unselected-character character col-2 p-3 m-3');
+            var h3 = $('<h3>');
+            h3.text(char.name);
+            newDiv.append(h3);
+            var img = $('<img/>');
+            img.attr('class', 'img-fluid character-image');
+            img.attr('src', src);
+            newDiv.append(img);
+            newDiv.data('name', name);
+            newDiv.data('hp', hp);
+            newDiv.data('minAtk', minAtk);
+            newDiv.data('maxAtk', maxAtk);
+            var hpDisplay = $('<div>');
+            hpDisplay.attr('class', 'hp-display');
+            hpDisplay.text('HP: ' + char.hp);
+            newDiv.append(hpDisplay);
             char.div = newDiv;
             return char;
         }
     };
-    game.characters.push( game.newCharacter( "Bob", 150, 'assets/images/bob.jpg', 10, 40 ) );
-    game.characters.push( game.newCharacter( "joe", 150, 'assets/images/bob.jpg', 10, 40 ) );
-    game.characters.push( game.newCharacter( "jim", 150, 'assets/images/bob.jpg', 10, 40 ) );
-    game.characters.push( game.newCharacter( "tim", 150, 'assets/images/bob.jpg', 10, 40 ) );
-    $.each( game.characters, function ( index, val ) {
-        unselectedCharactersContainer.append( val.div );
-    } );
-    $( '.unselected-character' ).click( function () {
-        $( this ).addClass( 'selected-character' );
-        $( this ).removeClass( 'unselected-character' );
-        activeBattleContainer.append( this );
-        $( this ).off();
-        $( '.unselected-character' ).off();
-        $( '.unselected-character' ).each( function ( index, val ) {
-            $( val ).addClass( 'unselected-enemy' );
-            $( val ).removeClass( 'unselected-character' );
-            unselectedEnemysContainer.append( val );
-        } );
-        $( '.unselected-enemy' ).click( function () {
-            $( this ).addClass( 'selected-enemy' );
-            $( this ).removeClass( 'unselected-enemy' );
-            console.log( $( this ).data( 'name' ) );
-            console.log( $( this ).data( 'hp' ) );
-            console.log( $( this ).data( 'minAtk' ) );
-            console.log( $( this ).data( 'maxAtk' ) );
-            activeBattleContainer.append( this );
-            $( this ).off();
-            $( '.unselected-enemy' ).off();
-        } );
-    } );
-} );
+    game.characters.push(game.createCharacter("Gundam", 150, 'assets/images/gundam.png', 10, 40));
+    game.characters.push(game.createCharacter("Voltron", 150, 'assets/images/voltron.png', 10, 40));
+    game.characters.push(game.createCharacter("Wall-e", 150, 'assets/images/walle.png', 10, 40));
+    game.characters.push(game.createCharacter("Zoids", 150, 'assets/images/zoid.jpg', 10, 40));
+    $.each(game.characters, function (index, val) {
+        game.domContainers.unselectedCharactersContainer.append(val.div);
+    });
+    $('.unselected-character').click(function () {
+        $('.unselected-character').off();
+        $(this).addClass('selected-character');
+        $(this).removeClass('unselected-character');
+        game.domContainers.activeBattleContainer.append(this);
+        $('.unselected-character').each(function (index, val) {
+            $(val).addClass('unselected-enemy');
+            $(val).removeClass('unselected-character');
+            game.domContainers.unselectedEnemysContainer.append(val);
+        });
+        $('.enemy-characters').removeClass('hidden');
+        $('.active-battle').removeClass('hidden');
+        $('.characters').addClass('hidden');
+        $('.unselected-enemy').click(function () {
+            $('.unselected-enemy').off();
+            $(this).addClass('selected-enemy');
+            $(this).removeClass('unselected-enemy');
+            console.log($(this).data('name'));
+            console.log($(this).data('hp'));
+            console.log($(this).data('minAtk'));
+            console.log($(this).data('maxAtk'));
+            game.domContainers.activeBattleContainer.append(this);
+        });
+    });
+});
